7bf3d629691b15b7352cc2aebc80abfa8a1973dc
Geoffrey Grosenbach <boss@topfunky.com>
2017-09-20 10:20:26 -0700

    Implementation of `gigasecond` exercise.

--- go/gigasecond/gigasecond.go
+++ go/gigasecond/gigasecond.go
@@ -1,10 +1,11 @@
 package gigasecond
 
-// import path for the time package from the standard library
 import "time"
 
 const testVersion = 4
 
-func AddGigasecond(time.Time) time.Time {
-	return time.Time.UTC().Second()
+// AddGigasecond adds one billion seconds to a given time.
+// It returns a new time representing the resulting point in time.
+func AddGigasecond(t time.Time) time.Time {
+	return t.Add(time.Second * time.Duration(1e9))
 }
