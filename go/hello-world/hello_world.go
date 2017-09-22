8c197a14ca04926a71b99d584547f925af7ccfda
Geoffrey Grosenbach <boss@topfunky.com>
2017-09-22 11:48:09 -0700

    Hello, world implementation

---
+++ go/hello-world/hello_world.go
@@ -0,0 +1,8 @@
+// Package greeting provides functions for building cordial textual messages.
+package greeting
+
+// HelloWorld takes no arguments.
+// It returns the classic beginner programming string "Hello, World."
+func HelloWorld() string {
+	return "Hello, World!"
+}
