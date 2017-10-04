b3a24a40b5c8e1452077b8ca7a8d3aab64d9898e
Geoffrey Grosenbach <boss@topfunky.com>
2017-10-04 12:47:42 -0700

    Implement bowling scoring logic

--- go/bowling/bowling.go
+++ go/bowling/bowling.go
@@ -1,123 +1,140 @@
 package bowling
 
 import (
-  "errors"
-  "fmt"
+	"errors"
 )
 
 const testVersion = 1
 
+const (
+	minPins         = 0
+	maxPins         = 10
+	finalFrameIndex = 9
+)
+
+// Game stores bowling activity and scores the result.
 type Game struct {
-  frames                [10][3]int
-  frameIndex, rollIndex int
+	frames                [10][3]int
+	frameIndex, rollIndex int
 }
 
+// NewGame creates a Game for scoring bowling.
 func NewGame() *Game {
-  return &Game{}
+	return &Game{}
 }
 
-func (g Game) String() string {
-  return fmt.Sprintf(
-    "{frames:%v frameIndex:%v rollIndex:%v}",
-    g.frames, g.frameIndex, g.rollIndex)
-  }
+// Roll records a throw of the ball.
+func (g *Game) Roll(pins int) error {
+	if err := g.applyRules(g.frames[g.frameIndex], g.frameIndex, g.rollIndex, pins); err != nil {
+		return err
+	}
 
-  func (g *Game) Roll(pins int) error {
-    if err := applyRules(g.frames[g.frameIndex], g.frameIndex, g.rollIndex, pins); err != nil {
-      return err
-    }
+	g.frames[g.frameIndex][g.rollIndex] = pins
+	g.rollIndex++
 
-    g.frames[g.frameIndex][g.rollIndex] = pins
-    g.rollIndex++
+	if g.frameIndex < finalFrameIndex && isStrike(g.frames[g.frameIndex]) {
+		g.rollIndex = 0
+		g.frameIndex++
+	} else if g.frameIndex < finalFrameIndex && g.rollIndex > 1 {
+		g.rollIndex = 0
+		g.frameIndex++
+	}
 
-    if g.frameIndex < 9 && isStrike(g.frames[g.frameIndex]) {
-      g.rollIndex = 0
-      g.frameIndex++
-    } else if g.frameIndex < 9 && g.rollIndex > 1 {
-      g.rollIndex = 0
-      g.frameIndex++
-    }
+	return nil
+}
 
-    return nil
-  }
+func (g Game) applyRules(frame [3]int, frameIndex, rollIndex, pins int) error {
+	switch {
+	case pins < minPins:
+		return errors.New("can't roll a negative number")
+	case pins > maxPins:
+		return errors.New("can't roll more than 10")
+	case frameIndex < finalFrameIndex && sum(frame)+pins > maxPins:
+		return errors.New("can't roll more than 10 in a single frame")
+	case frameIndex == finalFrameIndex && rollIndex == 2:
+		if isSpare(frame) {
+			return nil
+		} else if isStrike(frame) && frame[1] == maxPins {
+			return nil
+		} else if frame[1]+pins > maxPins {
+			return errors.New(
+				"can't roll more than 10 in the last two rolls of the 10th frame")
+		} else if frame[0]+frame[1] < maxPins {
+			return errors.New("the game is over; no more rolls are allowed")
+		}
+	}
+	return nil
+}
 
-  func applyRules(frame [3]int, frameIndex, rollIndex, pins int) error {
-    const (
-      minPins         = 0
-      maxPins         = 10
-      finalFrameIndex = 9
-    )
-    switch {
-    case pins < minPins:
-      return errors.New("Can't roll a negative number")
-    case pins > maxPins:
-      return errors.New("Can't roll more than 10")
-    case frameIndex < finalFrameIndex && sum(frame)+pins > 10:
-      return errors.New("Can't roll more than 10 in a single frame")
-    case frameIndex == finalFrameIndex && rollIndex == 2:
-      if isSpare(frame) {
-        return nil
-      } else if isStrike(frame) && frame[1] == 10 {
-        return nil
-      } else if frame[1]+pins > maxPins {
-        return errors.New("Can't roll more than 10 in the last two rolls of the 10th frame")
-      } else if frame[0]+frame[1] < 10 {
-        return errors.New("The game is over. No more rolls are allowed.")
-      }
-    }
-    return nil
-  }
+// Score calculates the number of points achived for all rolls.
+func (g Game) Score() (score int, e error) {
+	if !g.isGameDone() {
+		return 0, errors.New("game cannot be scored until it is over")
+	}
+	for i, frame := range g.frames {
+		if i == len(g.frames)-1 {
+			// Final frame
+			if frame == [3]int{maxPins, maxPins, maxPins} {
+				if isStrike(g.frames[i-1]) {
+					score += 20
+				} else {
+					score += 30
+				}
+			} else {
+				score += sum(frame)
+			}
+		} else {
+			// All frames but the final
+			score += sum(frame)
+			if isSpare(frame) {
+				score += g.frames[i+1][0]
+			} else if isStrike(frame) {
+				score += g.sumOfNextTwoRolls(i)
+			}
+		}
+	}
+	return score, nil
+}
 
-  func (g Game) Score() (score int, e error) {
-    for i, frame := range g.frames {
-      score += sum(frame)
-      if i < len(g.frames)-1 {
-        if isSpare(frame) {
-          score += g.frames[i+1][0]
-        } else if isStrike(frame) {
-          score += g.sumOfNextTwoRolls(i)
-        }
-      }
-    }
-    return score, nil
-  }
+func (g Game) isGameDone() bool {
+	if g.frameIndex < finalFrameIndex {
+		return false
+	}
+	finalFrame := g.frames[finalFrameIndex]
+	switch {
+	case isOpen(finalFrame) && g.rollIndex == 2:
+		return true
+	case (isStrike(finalFrame) || isSpare(finalFrame)) && g.rollIndex == 3:
+		return true
+	}
+	return false
+}
 
-  func sum(nums [3]int) (total int) {
-    for _, n := range nums {
-      total += n
-    }
-    return total
-  }
+// sum calculates the simple total of a single frame.
+func sum(nums [3]int) (total int) {
+	for _, n := range nums {
+		total += n
+	}
+	return total
+}
 
-  func isSpare(nums [3]int) bool {
-    if nums[0] < 10 && nums[0]+nums[1] == 10 {
-      return true
-    }
-    return false
-  }
+func isSpare(nums [3]int) bool {
+	return nums[0] < maxPins && nums[0]+nums[1] == maxPins
+}
 
-  func isOpen(nums [3]int) bool {
-    if nums[0] < 10 && nums[0]+nums[1] < 10 {
-      return true
-    }
-    return false
-  }
+func isOpen(nums [3]int) bool {
+	return nums[0] < maxPins && nums[0]+nums[1] < maxPins
+}
 
-  func isStrike(nums [3]int) bool {
-    if nums[0] == 10 {
-      return true
-    }
-    return false
-  }
+func isStrike(nums [3]int) bool {
+	return nums[0] == maxPins
+}
 
-  func (g Game) sumOfNextTwoRolls(index int) (total int) {
-    for i := index+1; i < len(g.frames)-1; i++ {
-      if isStrike(g.frames[i]) {
-        // Sum of this strike plus the next roll
-        return 10 + g.frames[i+1][0]
-      } else {
-        return sum(g.frames[i])
-      }
-    }
-    return total
-  }
+func (g Game) sumOfNextTwoRolls(index int) (total int) {
+	i := index + 1
+	if isStrike(g.frames[i]) && i < len(g.frames)-1 {
+		// Sum of this strike plus the next roll
+		return 10 + g.frames[i+1][0]
+	}
+	return sum(g.frames[i])
+}
